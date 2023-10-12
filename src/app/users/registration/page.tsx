0ad338feaf299a3e898614af295accaaa9caa7d9
Shaban-mern-developer <shabanamerndeveloper@gmail.com>
2023-10-12 11:34:01 -0700

    next commit

--- src/app/users/registration/page.tsx
+++ src/app/users/registration/page.tsx
@@ -1,15 +1,14 @@
 'use client'
-import Link from "next/link";
-import React, { useState } from "react";
-import { FaUser, FaLock } from 'react-icons/fa';
+import React, {useState} from 'react'
+import Link from 'next/link'
+import { FaUser, FaLock } from 'react-icons/fa'
 
-export default function Home() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const handleLogin = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
-    console.log('hello', email, password);
-  }
+const Registration = () => {
+    const [email, setEmail] = useState('');
+    const [ password, setPassword ] = useState('');
+    const handleLogin = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+    }
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className=" p-8 rounded w-72 bg-slate-200 shadow-md">
@@ -43,20 +42,13 @@ export default function Home() {
         </div>
       )} */}
           <button type="submit" className="bg-blue-900 text-white text-2xl rounded-md h-10 w-full">
-            Login
+            Signup
           </button>
         </form>
-        <Link href='/users/registration'
-          className="text-blue-500 cursor-pointer"
-        >
-          Don't have an <span>account.</span>
-        </Link>
-        <p
-          className="cursor-pointer text-blue-500"
-        >
-          Forgot Password?
-        </p>
+        
       </div>
     </main>
-  );
+  )
 }
+
+export default Registration
